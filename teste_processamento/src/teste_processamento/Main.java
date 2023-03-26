9196ca898325e39f5d881f0b78e94fa1f0e00446
fernando <fernan_fer_soar@hotmail.com>
2023-03-26 00:22:49 -0300

    atualizado pasta

---
+++ teste_processamento/src/teste_processamento/Main.java
@@ -0,0 +1,24 @@
+package teste_processamento;
+
+public class Main {
+public static void main (String[] args) {
+	
+	int c,d,f;
+	double b,B,h,area,resultado;
+	b= 6.0;
+	B= 8.0;
+	h= 5.0;
+	c= 5;
+	d= 2;
+	f=(int)b;
+			
+	resultado = (double) c / d;
+	area= (b+B)/2.0*h;
+	System.out.println(resultado);
+	System.out.println(area);
+	System.out.println(f);
+	
+	
+	
+}
+}
