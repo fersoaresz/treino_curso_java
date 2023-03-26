bcf064b5d6b227b302c629017d70cd5123abc1f0
fernando <fernan_fer_soar@hotmail.com>
2023-03-25 22:44:42 -0300

    adicionado texto e printf

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
 		System.out.println(x);
 		System.out.printf("%.2f%n",x);
 		System.out.printf("%.4f%n",x);
@@ -12,7 +15,8 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		System.out.printf("%.4f%n",x);
 		System.out.println("RESULTADO = "+ x + " METROS ");
-
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		System.out.printf("%s te %d anos e ganha R$ %.2f reais\n",nome,idade,renda);
 	}
 
 }
